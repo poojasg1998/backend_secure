027659d899db3f845f45d37a9f9b135de9940aa5
Pooja SG <114904291+poojasg1998@users.noreply.github.com>
2026-02-02 14:41:58 +0530

    'updated2'

--- models/fcm_tokens.js
+++ models/fcm_tokens.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
 
-const fcmtokensSchema = new mongoose.Schema({
-  name: String,
-  token: String
-});
+
+const fcmtokensSchema = new mongoose.Schema(
+  {
+    name: String,
+    token: String
+  },
+  { collection: 'FCM_Tokens' }
+);
 
 module.exports = mongoose.model('Fcmtoken', fcmtokensSchema);
