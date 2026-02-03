98f4bd3a915e9d1c0265704c6190fbe81f49a687
Pooja SG <114904291+poojasg1998@users.noreply.github.com>
2026-02-03 12:30:08 +0530

    'updated_unique_data3'

--- models/fcm_tokens.js
+++ models/fcm_tokens.js
@@ -1,38 +1,38 @@
-// const mongoose = require('mongoose');
-
-
-// const fcmtokensSchema = new mongoose.Schema(
-//   {
-//     name: String,
-//     token: String
-//   },
-//   { collection: 'FCM_Tokens' }
-// );
-
-// module.exports = mongoose.model('Fcmtoken', fcmtokensSchema);
-
 const mongoose = require('mongoose');
+
 
 const fcmtokensSchema = new mongoose.Schema(
   {
-    user_id: {
-      type: String,
-      ref: 'User',
-      required: true,
-    },
-    token: {
-      type: String,
-      required: true,
-    },
-    name: {
-      type: String,
-    },
+    name: String,
+    token: String
   },
-  { timestamps: true },
   { collection: 'FCM_Tokens' }
 );
 
-// 🔥 Compound unique index
-fcmtokensSchema.index({ user_id: 1, token: 1 }, { unique: true });
-
 module.exports = mongoose.model('Fcmtoken', fcmtokensSchema);
+
+// const mongoose = require('mongoose');
+
+// const fcmtokensSchema = new mongoose.Schema(
+//   {
+//     user_id: {
+//       type: String,
+//       ref: 'User',
+//       required: true,
+//     },
+//     token: {
+//       type: String,
+//       required: true,
+//     },
+//     name: {
+//       type: String,
+//     },
+//   },
+//   { timestamps: true },
+//   { collection: 'FCM_Tokens' }
+// );
+
+// // 🔥 Compound unique index
+// fcmtokensSchema.index({ user_id: 1, token: 1 }, { unique: true });
+
+// module.exports = mongoose.model('Fcmtoken', fcmtokensSchema);
